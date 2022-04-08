268e396dc78fd06419086494a8a2280415bab59a
V. Ganesh <tovganesh@yahoo.co.in>
2022-04-08 23:18:17 +0530

    Fix: column types not honoured when using dbt seeds (#18)
    See: Issue#17 https://github.com/cloudera/dbt-impala/issues/17
    Internal Ticket: https://jira.cloudera.com/browse/DBT-78
    
    Testplan: Please follow the steps in Issue#17

---
+++ dbt/include/impala/macros/seed.sql
@@ -0,0 +1,38 @@
+{# based on spark adapter: https://github.com/dbt-labs/dbt-spark/pull/166/files #}
+{% macro impala__load_csv_rows(model, agate_table) %}
+
+  {% set batch_size = get_batch_size() %}
+  {% set column_override = model['config'].get('column_types', {}) %}
+
+  {% set statements = [] %}
+
+  {% for chunk in agate_table.rows | batch(batch_size) %}
+      {% set bindings = [] %}
+
+      {% for row in chunk %}
+          {% do bindings.extend(row) %}
+      {% endfor %}
+
+      {% set sql %}
+          insert into {{ this.render() }} values
+          {% for row in chunk -%}
+              ({%- for col_name in agate_table.column_names -%}
+                  {%- set inferred_type = adapter.convert_type(agate_table, loop.index0) -%}
+                  {%- set type = column_override.get(col_name, inferred_type) -%}
+                    cast({{ get_binding_char() }} as {{type}})
+                  {%- if not loop.last%},{%- endif %}
+              {%- endfor -%})
+              {%- if not loop.last%},{%- endif %}
+          {%- endfor %}
+      {% endset %}
+
+      {% do adapter.add_query(sql, bindings=bindings, abridge_sql_log=True) %}
+
+      {% if loop.index0 == 0 %}
+          {% do statements.append(sql) %}
+      {% endif %}
+  {% endfor %}
+
+  {# Return SQL #}
+  {{ return(statements[0]) }}
+{% endmacro %}
