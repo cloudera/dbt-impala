b2c161312d82b73f54ba487ed10801e5f61b3634
V. Ganesh <tovganesh@yahoo.co.in>
2022-08-02 16:43:48 +0530

    New adapter test suite integration (needs dbt-core=1.1) (#33)
    * New adapter test suite integration (needs dbt-core=1.1)
    
    * Added fix for not escaping interval timestamp and not to swlalow exception. Refer Jira-86
    
    * Fix duplicate code in get_columns_in_relation while merging changes
    
    Co-authored-by: Tapas Senapati <tksenapa@ncsu.edu>

---
+++ tests/conftest.py
@@ -0,0 +1,28 @@
+import pytest
+import os
+
+# Import the standard functional fixtures as a plugin
+# Note: fixtures with session scope need to be local
+pytest_plugins = ["dbt.tests.fixtures.project"]
+
+impala_ldap = {
+        'type': 'impala',
+        'threads': 5,
+        'dbname': 'dbttest',
+        'schema': 'dbttest',
+        'host': 'coordinator-dbt-impala.dw-ciadev.cna2-sx9y.cloudera.site',
+        'http_path': 'cliservice',
+        'port': 443,
+        'auth_type': 'ldap',
+        'use_http_transport': True,
+        'use_ssl': True,
+        'username': 'ganesh.venkateshwara',
+        'password': 'Password123!'
+    }
+
+# The profile dictionary, used to write out profiles.yml
+# dbt will supply a unique schema per test, so we do not specify 'schema' here
+@pytest.fixture(scope="class")
+def dbt_profile_target():
+    return impala_ldap
+
