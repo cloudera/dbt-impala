8aedd16758c70eaf183a7a9061ffce64da298c28
Tapas Kumar Senapati <tksenapa@ncsu.edu>
2022-09-12 19:04:05 +0530

    Fix typos and refactor code to pep-8 standards

--- dbt/adapters/impala/connections.py
+++ dbt/adapters/impala/connections.py
@@ -28,7 +28,7 @@ from dbt.contracts.connection import Connection, AdapterResponse
 from dbt.events.functions import fire_event
 from dbt.events.types import ConnectionUsed, SQLQuery, SQLQueryStatus
 
-from dbt.logger import GLOBAL_LOGGER as logger
+from dbt.logger import GLOBAL_LOGGER as LOGGER
 
 import impala.dbapi
 
@@ -42,6 +42,7 @@ DEFAULT_IMPALA_HOST = "localhost"
 DEFAULT_IMPALA_PORT = 21050
 DEFAULT_MAX_RETRIES = 3
 
+
 @dataclass
 class ImpalaCredentials(Credentials):
     host: str = DEFAULT_IMPALA_HOST
@@ -54,34 +55,27 @@ class ImpalaCredentials(Credentials):
     kerberos_service_name: Optional[str] = None
     use_http_transport: Optional[bool] = True
     use_ssl: Optional[bool] = True
-    http_path: Optional[str] = ''  # for supporing a knox proxy in ldap env
-    usage_tracking: Optional[bool] = True # usage tracking is enabled by default
+    http_path: Optional[str] = ""  # for supporting a knox proxy in ldap env
+    usage_tracking: Optional[bool] = True  # usage tracking is enabled by default
     retries: Optional[int] = DEFAULT_MAX_RETRIES
 
-    _ALIASES = {
-        'dbname':'database',
-        'pass':'password',
-        'user':'username'
-    }
+    _ALIASES = {"dbname": "database", "pass": "password", "user": "username"}
 
     @classmethod
     def __pre_deserialize__(cls, data):
         data = super().__pre_deserialize__(data)
-        if 'database' not in data:
-            data['database'] = None
+        if "database" not in data:
+            data["database"] = None
         return data
 
     def __post_init__(self):
         # impala classifies database and schema as the same thing
-        if (
-            self.database is not None and
-            self.database != self.schema
-        ):
+        if self.database is not None and self.database != self.schema:
             raise dbt.exceptions.RuntimeException(
-                f'    schema: {self.schema} \n'
-                f'    database: {self.database} \n'
-                f'On Impala, database must be omitted or have the same value as'
-                f' schema.'
+                f"    schema: {self.schema} \n"
+                f"    database: {self.database} \n"
+                f"On Impala, database must be omitted or have the same value as"
+                f" schema."
             )
         self.database = None
 
@@ -90,12 +84,12 @@ class ImpalaCredentials(Credentials):
 
     @property
     def type(self):
-        return 'impala'
+        return "impala"
 
     def _connection_keys(self):
         # return an iterator of keys to pretty-print in 'dbt debug'.
         # Omit fields like 'password'!
-        return ('host', 'port', 'schema', 'username')
+        return "host", "port", "schema", "username"
 
     @property
     def unique_field(self) -> str:
@@ -104,23 +98,23 @@ class ImpalaCredentials(Credentials):
 
 
 class ImpalaConnectionManager(SQLConnectionManager):
-    TYPE = 'impala'
+    TYPE = "impala"
 
     @contextmanager
     def exception_handler(self, sql: str):
         try:
             yield
         except impala.dbapi.DatabaseError as exc:
-            logger.debug('dbt-imapla error: {}'.format(str(e)))
+            LOGGER.debug("dbt-impala error: {}".format(str(e)))
             raise dbt.exceptions.DatabaseException(str(exc))
         except Exception as exc:
-            logger.debug("Error running SQL: {}".format(sql))
+            LOGGER.debug("Error running SQL: {}".format(sql))
             raise dbt.exceptions.RuntimeException(str(exc))
 
     @classmethod
     def open(cls, connection):
-        if connection.state == 'open':
-            logger.debug('Connection is already open, skipping open.')
+        if connection.state == "open":
+            LOGGER.debug("Connection is already open, skipping open.")
             return connection
 
         credentials = connection.credentials
@@ -128,52 +122,59 @@ class ImpalaConnectionManager(SQLConnectionManager):
         auth_type = "insecure"
 
         try:
-            # the underlying dbapi supports retries, so this is directly used instead to support retries 
-            if (credentials.auth_type == "LDAP" or credentials.auth_type == "ldap"): # ldap connection
+            # the underlying dbapi supports retries, so this is directly used instead to support retries
+            if (
+                    credentials.auth_type == "LDAP" or credentials.auth_type == "ldap"
+            ):  # ldap connection
                 handle = impala.dbapi.connect(
                     host=credentials.host,
                     port=credentials.port,
-                    auth_mechanism='LDAP',
+                    auth_mechanism="LDAP",
                     use_http_transport=credentials.use_http_transport,
                     user=credentials.username,
                     password=credentials.password,
                     use_ssl=credentials.use_ssl,
                     http_path=credentials.http_path,
-                    retries=credentials.retries
+                    retries=credentials.retries,
                 )
                 auth_type = "ldap"
-            elif (credentials.auth_type == "GSSAPI" or credentials.auth_type == "gssapi" or credentials.auth_type == "kerberos"): # kerberos based connection
+            elif (
+                    credentials.auth_type == "GSSAPI"
+                    or credentials.auth_type == "gssapi"
+                    or credentials.auth_type == "kerberos"
+            ):  # kerberos based connection
                 handle = impala.dbapi.connect(
                     host=credentials.host,
                     port=credentials.port,
-                    auth_mechanism='GSSAPI',
+                    auth_mechanism="GSSAPI",
                     kerberos_service_name=credentials.kerberos_service_name,
                     use_http_transport=credentials.use_http_transport,
                     use_ssl=credentials.use_ssl,
-                    retries=credentials.retries
+                    retries=credentials.retries,
                 )
                 auth_type = "kerberos"
-            else: # default, insecure connection
+            else:  # default, insecure connection
                 handle = impala.dbapi.connect(
                     host=credentials.host,
                     port=credentials.port,
-                    retries=credentials.retries
+                    retries=credentials.retries,
                 )
 
-            connection.state = 'open'
+            connection.state = "open"
             connection.handle = handle
-        except:
-            logger.debug("Connection error")
-            connection.state = 'fail'
+        except Exception as ex:
+            LOGGER.debug("Connection error {}".format(ex))
+            connection.state = "fail"
             connection.handle = None
             pass
 
         # track usage
-        payload = {}
-        payload["id"] = "dbt_impala_open"
-        payload["unique_hash"] = hashlib.md5(credentials.host.encode()).hexdigest()
-        payload["auth"] = auth_type
-        payload["connection_state"] = connection.state
+        payload = {
+            "id": "dbt_impala_open",
+            "unique_hash": hashlib.md5(credentials.host.encode()).hexdigest(),
+            "auth": auth_type,
+            "connection_state": connection.state,
+        }
 
         tracker.track_usage(payload)
 
@@ -181,34 +182,32 @@ class ImpalaConnectionManager(SQLConnectionManager):
 
     @classmethod
     def get_response(cls, cursor):
-        message = 'OK'
-        return AdapterResponse(
-            _message=message
-        )
+        message = "OK"
+        return AdapterResponse(_message=message)
 
     def cancel(self, connection):
         connection.handle.close()
 
     def add_begin_query(self, *args, **kwargs):
-        logger.debug("NotImplemented: add_begin_query")
+        LOGGER.debug("NotImplemented: add_begin_query")
 
     def add_commit_query(self, *args, **kwargs):
-        logger.debug("NotImplemented: add_commit_query")
+        LOGGER.debug("NotImplemented: add_commit_query")
 
     def commit(self, *args, **kwargs):
-        logger.debug("NotImplemented: commit")
+        LOGGER.debug("NotImplemented: commit")
 
     def rollback(self, *args, **kwargs):
-        logger.debug("NotImplemented: rollback")
+        LOGGER.debug("NotImplemented: rollback")
 
     def add_query(
-        self,
-        sql: str,
-        auto_begin: bool = True,
-        bindings: Optional[Any] = None,
-        abridge_sql_log: bool = False
+            self,
+            sql: str,
+            auto_begin: bool = True,
+            bindings: Optional[Any] = None,
+            abridge_sql_log: bool = False,
     ) -> Tuple[Connection, Any]:
-        
+
         connection = self.get_thread_connection()
         if auto_begin and connection.transaction_open is False:
             self.begin()
@@ -216,7 +215,7 @@ class ImpalaConnectionManager(SQLConnectionManager):
 
         with self.exception_handler(sql):
             if abridge_sql_log:
-                log_sql = '{}...'.format(sql[:512])
+                log_sql = "{}...".format(sql[:512])
             else:
                 log_sql = sql
 
@@ -225,19 +224,17 @@ class ImpalaConnectionManager(SQLConnectionManager):
 
             cursor = connection.handle.cursor()
 
-            # paramstlye parameter is needed for the datetime object to be correctly qouted when
+            # paramstyle parameter is needed for the datetime object to be correctly quoted when
             # running substitution query from impyla. this fix also depends on a patch for impyla:
             # https://github.com/cloudera/impyla/pull/486
-            configuration = {}
-            configuration['paramstyle'] = 'format'
+            configuration = {"paramstyle": "format"}
             cursor.execute(sql, bindings, configuration)
 
             fire_event(
                 SQLQueryStatus(
                     status=str(self.get_response(cursor)),
-                    elapsed=round((time.time() - pre), 2)
+                    elapsed=round((time.time() - pre), 2),
                 )
             )
 
             return connection, cursor
-
