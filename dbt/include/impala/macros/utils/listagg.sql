11f3e8c3a40a12dad6092deb5766ce5f4e87669d
V. Ganesh <ganesh.venkateshwara@cloudera.com>
2022-10-21 17:44:16 +0530

    listagg cannot be fully implemented currently in impala, upsteream has an open ticket on supporting this https://issues.apache.org/jira/browse/IMPALA-5545

--- dbt/include/impala/macros/utils/listagg.sql
+++ dbt/include/impala/macros/utils/listagg.sql
@@ -1,17 +1,15 @@
 {% macro impala__listagg(measure, delimiter_text, order_by_clause, limit_num) -%}
 
+  {# see https://issues.apache.org/jira/browse/IMPALA-5545 #}
   {% if order_by_clause %}
-    {{ exceptions.warn("order_by_clause is not supported for listagg on Spark/Databricks") }}
+    {{ exceptions.warn("order_by_clause is not supported for listagg on Impala") }}
+  {% endif %}
+  {% if limit_num %}
+    {{ exceptions.warn("limit_num is not supported for listagg on Impala") }}
   {% endif %}
 
-  {% set collect_list %} collect_list({{ measure }}) {% endset %}
+  {% set collect_list %} group_concat({{ measure }}, {{ delimiter_text }}) {% endset %}
 
-  {% set limited %} slice({{ collect_list }}, 1, {{ limit_num }}) {% endset %}
-
-  {% set collected = limited if limit_num else collect_list %}
-
-  {% set final %} array_join({{ collected }}, {{ delimiter_text }}) {% endset %}
-
-  {% do return(final) %}
+  {% do return(collect_list) %}
 
 {%- endmacro %}
