732a9c92b70ebe300a8f2f69285876b41cdb98ce
V. Ganesh <ganesh.venkateshwara@cloudera.com>
2022-11-18 20:12:49 +0530

    use the refactored tracking lib: event type, warehouse version

--- dbt/adapters/impala/relation.py
+++ dbt/adapters/impala/relation.py
@@ -42,7 +42,7 @@ class ImpalaRelation(BaseRelation):
     def __post_init__(self):
         if (self.type):
             tracker.track_usage({
-                "event_type": "dbt_impala_model_access",
+                "event_type": tracker.TrackingEventType.model_access.name,
                 "model_name": self.render(),
                 "model_type": self.type,
                 "incremental_strategy": ""
@@ -54,7 +54,7 @@ class ImpalaRelation(BaseRelation):
     def log_relation(self, incremental_strategy):
         if (self.type):
             tracker.track_usage({
-                "event_type": "dbt_impala_new_incremental",
+                "event_type": tracker.TrackingEventType.new_incremental.name,
                 "model_name": self.render(),
                 "model_type": self.type,
                 "incremental_strategy": incremental_strategy
