6c45f5a772d4a413419897cf084a2bba6561bb9e
vamshikolanu <vamshi.kolanu@gmail.com>
2023-04-25 14:23:30 -0700

    test for iceberg

--- tests/functional/adapter/test_formats.py
+++ tests/functional/adapter/test_formats.py
@@ -14,11 +14,21 @@
 
 import pytest
 
+from dbt.tests.util import (
+    run_dbt,
+    check_result_nodes_by_name,
+    relation_from_name,
+    check_relation_types
+)
+
 from tests.functional.adapter.test_basic import (
+    TestSimpleMaterializationsImpala,
     TestIncrementalImpala
 )
 
 from dbt.tests.adapter.basic.files import (
+    seeds_base_csv,
+    base_view_sql,
     schema_base_yml
 )
 
@@ -30,9 +40,133 @@ incremental_iceberg_sql = """
  {% endif %}
 """.strip()
 
+# For iceberg table formats, check_relations_equal macros is not working as expected
+# Impala upstream issue: https://issues.apache.org/jira/browse/IMPALA-12097
+# Hence removing this check from unit tests
+
 class TestIncrementalIcebergFormatImpala(TestIncrementalImpala):
     @pytest.fixture(scope="class")
     def models(self):
         return {"incremental_test_model.sql": incremental_iceberg_sql, "schema.yml": schema_base_yml}
- 
+
+    def test_incremental(self, project):
+        # seed command
+        results = run_dbt(["seed"])
+        assert len(results) == 2
+
+        # base table rowcount
+        relation = relation_from_name(project.adapter, "base")
+        result = project.run_sql(f"select count(*) as num_rows from {relation}", fetch="one")
+        assert result[0] == 10
+
+        # added table rowcount
+        relation = relation_from_name(project.adapter, "added")
+        result = project.run_sql(f"select count(*) as num_rows from {relation}", fetch="one")
+        assert result[0] == 20
+
+        # run command
+        # the "seed_name" var changes the seed identifier in the schema file
+        results = run_dbt(["run", "--vars", "seed_name: base"])
+        assert len(results) == 1
+
+        # change seed_name var
+        # the "seed_name" var changes the seed identifier in the schema file
+        results = run_dbt(["run", "--vars", "seed_name: added"])
+        assert len(results) == 1
+
+        # get catalog from docs generate
+        catalog = run_dbt(["docs", "generate"])
+        assert len(catalog.nodes) == 3
+        assert len(catalog.sources) == 1
+
+
+iceberg_base_table_sql = """
+{{
+  config(
+    materialized="table",
+    iceberg=true
+  )
+}}
+select * from {{ source('raw', 'seed') }}
+"""
+
+iceberg_base_materialized_var_sql = """
+{{ 
+  config(
+    materialized=var("materialized_var", "table"),
+    iceberg=true
+  )
+}}
+select * from {{ source('raw', 'seed') }}
+"""
+
+class TestSimpleMaterializationsIcebergFormatImpala(TestSimpleMaterializationsImpala):
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "view_model.sql": base_view_sql,
+            "table_model.sql": iceberg_base_table_sql,
+            "swappable.sql": iceberg_base_materialized_var_sql,
+            "schema.yml": schema_base_yml,
+        }
+    
+    def test_base(self, project):
+
+        # seed command
+        results = run_dbt(["seed"])
+        # seed result length
+        assert len(results) == 1
+
+        # run command
+        results = run_dbt()
+        # run result length
+        assert len(results) == 3
+
+        # names exist in result nodes
+        check_result_nodes_by_name(results, ["view_model", "table_model", "swappable"])
+
+        # check relation types
+        expected = {
+            "base": "table",
+            "view_model": "view",
+            "table_model": "table",
+            "swappable": "table",
+        }
+        check_relation_types(project.adapter, expected)
+
+        # base table rowcount
+        relation = relation_from_name(project.adapter, "base")
+        result = project.run_sql(f"select count(*) as num_rows from {relation}", fetch="one")
+        assert result[0] == 10
+
+        # check relations in catalog
+        catalog = run_dbt(["docs", "generate"])
+        assert len(catalog.nodes) == 4
+        assert len(catalog.sources) == 1
+
+        # run_dbt changing materialized_var to view
+        results = run_dbt(["run", "-m", "swappable", "--vars", "materialized_var: view"])
+        assert len(results) == 1
+
+        # check relation types, swappable is view
+        expected = {
+            "base": "table",
+            "view_model": "view",
+            "table_model": "table",
+            "swappable": "view",
+        }
+        check_relation_types(project.adapter, expected)
+        
+        # run_dbt changing materialized_var to incremental
+        results = run_dbt(["run", "-m", "swappable", "--vars", "materialized_var: incremental"])
+        assert len(results) == 1
+
+        # check relation types, swappable is table
+        expected = {
+            "base": "table",
+            "view_model": "view",
+            "table_model": "table",
+            "swappable": "table",
+        }
+        check_relation_types(project.adapter, expected)
 
