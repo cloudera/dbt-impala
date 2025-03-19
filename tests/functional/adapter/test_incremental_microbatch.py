975e3832b7b97394332fdd186d112627ea25ef5f
Niranjan Subramanian <niru@cloudera.com>
2025-03-19 16:54:25 +0530

    DBT-808 Added a new negative test case for the microbatch support.

--- tests/functional/adapter/test_incremental_microbatch.py
+++ tests/functional/adapter/test_incremental_microbatch.py
@@ -14,7 +14,12 @@
 
 import pytest
 
-from dbt.tests.adapter.incremental.test_incremental_microbatch import BaseMicrobatch
+from dbt.tests.util import run_dbt_and_capture
+
+from dbt.tests.adapter.incremental.test_incremental_microbatch import (
+    BaseMicrobatch,
+    patch_microbatch_end_time,
+)
 
 _input_timestamp_modified_model_sql = """
 {{ config(materialized='table', event_time='event_time') }}
@@ -26,7 +31,25 @@ select 3 as id, to_timestamp('2020-01-03 00:00:00', 'yyyy-MM-dd HH:mm:ss') as ev
 """
 
 _microbatch_uniqueid_modified_model_sql = """
-{{ config(materialized='incremental', incremental_strategy='microbatch', event_time='event_time', batch_size='day', partition_by='date_day', begin=modules.datetime.datetime(2020, 1, 1, 0, 0, 0)) }}
+{{ config(
+    materialized='incremental',
+    incremental_strategy='microbatch',
+    event_time='event_time',
+    batch_size='day',
+    partition_by='date_day',
+    begin=modules.datetime.datetime(2020, 1, 1, 0, 0, 0))
+}}
+select *, to_date(event_time) as date_day from {{ ref('input_model') }}
+"""
+
+_microbatch_no_partition_by_sql = """
+{{ config(
+    materialized='incremental',
+    incremental_strategy='microbatch',
+    event_time='event_time',
+    batch_size='day',
+    begin=modules.datetime.datetime(2020, 1, 1, 0, 0, 0))
+}}
 select *, to_date(event_time) as date_day from {{ ref('input_model') }}
 """
 
@@ -46,3 +69,20 @@ class TestImpalaMicrobatch(BaseMicrobatch):
             database=project.database, schema=project.test_schema
         )
         return f"insert into {test_schema_relation}.input_model (id, event_time) values (4, to_timestamp('2020-01-04 00:00:00', 'yyyy-MM-dd HH:mm:ss')), (5, to_timestamp('2020-01-05 00:00:00', 'yyyy-MM-dd HH:mm:ss'))"
+
+
+class TestImpalaMicroBatchNoPartitionKey:
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "microbatch.sql": _microbatch_no_partition_by_sql,
+            "input_model.sql": _input_timestamp_modified_model_sql,
+        }
+
+    def test_no_partition_by(self, project):
+        with patch_microbatch_end_time("2020-01-03 13:57:00"):
+            _, stdout = run_dbt_and_capture(["run"], expect_pass=False)
+        assert (
+            "dbt-impala 'microbatch' incremental strategy requires a `partition_by` config"
+            in stdout
+        )
